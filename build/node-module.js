cf71d8aaa795462d7f024bac8da369f1e19d8a89
rali-udem <critter@iro.umontreal.ca>
2018-08-24 17:34:45 -0400

    Added node-module.js component file.

---
+++ build/node-module.js
@@ -0,0 +1,31 @@
+//// exports pour node.js
+//  Terminaux
+exports.N=N;
+exports.A=A;
+exports.Pro=Pro;
+exports.D=D;
+exports.V=V;
+exports.Adv=Adv;
+exports.P=P;
+exports.C=C;
+exports.Q=Q;
+// Syntagmes
+exports.S=S;
+exports.SP=SP;
+exports.CP=CP;
+exports.VP=VP;
+exports.NP=NP;
+exports.AP=AP;
+exports.PP=PP;
+exports.AdvP=AdvP;
+
+exports.DT=DT; // dates
+exports.NO=NO; // nombres
+
+exports.addToLexicon=addToLexicon;
+exports.getLemma=getLemma;
+
+if (typeof lexiconEn !== "undefined") exports.lexiconEn=lexiconEn;
+if (typeof loadEn    !== "undefined") exports.loadEn=loadEn;
+if (typeof lexiconFr !== "undefined") exports.lexiconFr=lexiconFr;
+if (typeof loadFr    !== "undefined") exports.loadFr=loadFr;
