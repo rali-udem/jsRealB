5f0e934614ecc23c49f8f9ffe765b353023e6d4a
Guy Lapalme <lapalme@iro.umontreal.ca>
2020-03-12 14:57:22 -0400

    Create rule-fr-pro.js

---
+++ data/rule-fr-pro.js
@@ -0,0 +1,348 @@
+var ruleFrPro = {
+    "pn4": {
+        "ending": "moi",
+        "declension": [{
+            "val": "moi","g": "x","n": "s","pe": 1, "tn":true
+        },{
+            "val": "moi-même","g": "x","n": "p","pe": 1, "tn":"refl"
+        },{
+            "val": "je","g": "x","n": "p","pe": 1, "c":"nom"
+        },{
+            "val": "me","g": "x","n": "p","pe": 1, "c":"acc"
+        },{
+            "val": "me","g": "x","n": "p","pe": 1, "c":"dat"
+        },{
+            "val": "me","g": "x","n": "p","pe": 1, "c":"refl"
+        },{
+            "val": "nous","g": "x","n": "p","pe": 1, "tn":true
+        },{
+            "val": "nous-mêmes","g": "x","n": "p","pe": 1, "tn":"refl"
+        },{
+            "val": "nous","g": "x","n": "p","pe": 1, "c":"nom"
+        },{
+            "val": "nous","g": "x","n": "p","pe": 1, "c":"acc"
+        },{
+            "val": "nous","g": "x","n": "p","pe": 1, "c":"dat"
+        },{
+            "val": "nous","g": "x","n": "p","pe": 1, "c":"refl"
+        },{
+            "val": "toi","g": "x","n": "s","pe": 2, "tn":true
+        },{
+            "val": "toi-même","g": "x","n": "s","pe": 2, "tn":"refl"
+        },{
+            "val": "tu","g": "x","n": "s","pe": 2, "c":"nom"
+        },{
+            "val": "te","g": "x","n": "s","pe": 2, "c":"acc"
+        },{
+            "val": "te","g": "x","n": "s","pe": 2, "c":"dat"
+        },{
+            "val": "te","g": "x","n": "s","pe": 2, "c":"refl"
+        },{
+            "val": "vous-mêmes","g": "x","n": "p","pe": 2, "tn":true
+        },{
+            "val": "vous","g": "x","n": "p","pe": 2, "tn":"refl"
+        },{
+            "val": "vous","g": "x","n": "p","pe": 2, "c":"nom"
+        },{
+            "val": "vous","g": "x","n": "p","pe": 2, "c":"acc"
+        },{
+            "val": "vous","g": "x","n": "p","pe": 2, "c":"dat"
+        },{
+            "val": "vous","g": "x","n": "p","pe": 2, "c":"refl"
+        },{
+            "val": "lui","g": "m","n": "s","pe": 3, "tn":true
+        },{
+            "val": "lui-même","g": "m","n": "s","pe": 3, "tn":"refl"
+        },{
+            "val": "il","g": "m","n": "s","pe": 3, "c":"nom"
+        },{
+            "val": "le","g": "m","n": "s","pe": 3, "c":"acc"
+        },{
+            "val": "lui","g": "m","n": "s","pe": 3, "c":"dat"
+        },{
+            "val": "se","g": "m","n": "s","pe": 3, "c":"refl"
+        },{
+            "val": "elle","g": "f","n": "s","pe": 3, "tn":true
+        },{
+            "val": "elle-même","g": "f","n": "s","pe": 3, "tn":"refl"
+        },{
+            "val": "elle","g": "f","n": "s","pe": 3, "c":"nom"
+        },{
+            "val": "la","g": "f","n": "s","pe": 3, "c":"acc"
+        },{
+            "val": "lui","g": "f","n": "s","pe": 3, "c":"dat"
+        },{
+            "val": "se","g": "f","n": "s","pe": 3, "c":"refl"
+        },{
+            "val": "soi","g": "n","n": "s","pe": 3, "tn":true
+        },{
+            "val": "soi-même","g": "n","n": "s","pe": 3, "tn":"refl"
+        },{
+            "val": "on","g": "n","n": "s","pe": 3, "c":"nom"
+        },{
+            "val": "le","g": "n","n": "s","pe": 3, "c":"acc"
+        },{
+            "val": "soi","g": "n","n": "s","pe": 3, "c":"dat"
+        },{
+            "val": "se","g": "f","n": "s","pe": 3, "c":"refl"
+        },{
+            "val": "eux","g": "m","n": "p","pe": 3, "tn":true
+        },{
+            "val": "eux-mêmes","g": "m","n": "p","pe": 3, "tn":"refl"
+        },{
+            "val": "ils","g": "m","n": "p","pe": 3, "c":"nom"
+        },{
+            "val": "les","g": "m","n": "p","pe": 3, "c":"acc"
+        },{
+            "val": "leur","g": "m","n": "p","pe": 3, "c":"dat"
+        },{
+            "val": "se","g": "m","n": "p","pe": 3, "c":"refl"
+        },{
+            "val": "elles","g": "f","n": "p","pe": 3, "tn":true
+        },
+            "val": "elles-mêmes","g": "f","n": "p","pe": 3, "tn":"refl"
+        },
+            "val": "elles","g": "f","n": "p","pe": 3, "c":"nom"
+        },
+            "val": "les","g": "f","n": "p","pe": 3, "c":"acc"
+        },
+            "val": "leur","g": "f","n": "p","pe": 3, "c":"dat"
+        },
+            "val": "se","g": "f","n": "p","pe": 3, "c":"refl"
+        }]
+    },
+    "pn4-2s": {
+        "ending": "toi",
+        "declension": [{
+            "val": "toi", "tn":true
+        },{
+            "val": "toi-même", "tn":"refl"
+        },{
+            "val": "tu", "c":"nom"
+        },{
+            "val": "te", "c":"acc"
+        },{
+            "val": "te", "c":"dat"
+        },{
+            "val": "te", "c":"refl"
+        }]
+    },
+    "pn4-3sm": {
+        "ending": "lui",
+        "declension": [{
+            "val": "lui", "tn":true
+        },{
+            "val": "lui-même", "tn":"refl"
+        },{
+            "val": "il", "c":"nom"
+        },{
+            "val": "le", "c":"acc"
+        },{
+            "val": "lui", "c":"dat"
+        },{
+            "val": "se", "c":"refl"
+        }]
+    },
+    "pn4-3sf": {
+        "ending": "elle",
+        "declension": [{
+            "val": "elle", "tn":true
+        },{
+            "val": "elle-même", "tn":"refl"
+        },{
+            "val": "elle", "c":"nom"
+        },{
+            "val": "la", "c":"acc"
+        },{
+            "val": "lui", "c":"dat"
+        },{
+            "val": "se", "c":"refl"
+        }]
+    },
+    "pn4-3sn": {
+        "ending": "on",
+        "declension": [{
+            "val": "on", "tn":true
+        },{
+            "val": "soi-même", "tn":"refl"
+        },{
+            "val": "on", "c":"nom"
+        },{
+            "val": "le", "c":"acc"
+        },{
+            "val": "soi", "c":"dat"
+        },{
+            "val": "se", "c":"refl"
+        }]
+    },
+    "pn4-1p": {
+        "ending": "nous",
+        "declension": [{
+            "val": "nous", "tn":true
+        },{
+            "val": "nous-mêmes", "tn":"refl"
+        },{
+            "val": "nous", "c":"nom"
+        },{
+            "val": "nous", "c":"acc"
+        },{
+            "val": "nous", "c":"dat"
+        },{
+            "val": "nous", "c":"refl"
+        }]
+    },
+    "pn4-2p": {
+        "ending": "toi",
+        "declension": [{
+            "val": "vous", "tn":true
+        },{
+            "val": "vous-mêmes", "tn":"refl"
+        },{
+            "val": "vous", "c":"nom"
+        },{
+            "val": "vous", "c":"acc"
+        },{
+            "val": "vous", "c":"dat"
+        },{
+            "val": "vous", "c":"refl"
+        }]
+    },
+    "pn4-3pm": {
+        "ending": "eux",
+        "declension": [{
+            "val": "eux", "tn":true
+        },{
+            "val": "eux-mêmes", "tn":"refl"
+        },{
+            "val": "ils", "c":"nom"
+        },{
+            "val": "les", "c":"acc"
+        },{
+            "val": "leur", "c":"dat"
+        },{
+            "val": "se", "c":"refl"
+        }]
+    },
+    "pn4-3pf": {
+        "ending": "elles",
+        "declension": [{
+            "val": "elles", "tn":true
+        },{
+            "val": "elles-mêmes", "tn":"refl"
+        },{
+            "val": "elles", "c":"nom"
+        },{
+            "val": "les", "c":"acc"
+        },{
+            "val": "leur", "c":"dat"
+        },{
+            "val": "se", "c":"refl"
+        }]
+    },
+    
+    "pn12": {
+        "ending": "mien",
+        "declension": [{
+            "val": "mien","g": "m","n": "s","pe": 1
+        },{
+            "val": "mienne","g": "f","n": "s","pe": 1
+        },{
+            "val": "miens","g": "m","n": "p","pe": 1
+        },{
+            "val": "miennes","g": "f","n": "p","pe": 1
+        },{
+            "val": "tien","g": "m","n": "s","pe": 2
+        },{
+            "val": "tienne","g": "f","n": "s","pe": 2
+        },{
+            "val": "tiens","g": "m","n": "p","pe": 2
+        },{
+            "val": "tiennes","g": "f","n": "p","pe": 2
+        },{
+            "val": "sien","g": "m","n": "s","pe": 3
+        },{
+            "val": "sienne","g": "f","n": "s","pe": 3
+        },{
+            "val": "siens","g": "m","n": "p","pe": 3
+        },{
+            "val": "siennes","g": "f","n": "p","pe": 3
+        }]
+    },
+    "pn12-2": {
+        "ending": "tien",
+        "declension": [{
+            "val": "tien","g": "m","n": "s"
+        },{
+            "val": "tienne","g": "f","n": "s"
+        },{
+            "val": "tiens","g": "m","n": "p"
+        },{
+            "val": "tiennes","g": "f","n": "p"
+        }]
+    },
+    "pn12-3": {
+        "ending": "sien",
+        "declension": [{
+            "val": "sien","g": "m","n": "s"
+        },{
+            "val": "sienne","g": "f","n": "s"
+        },{
+            "val": "siens","g": "m","n": "p"
+        },{
+            "val": "siennes","g": "f","n": "p"
+        }]
+    },
+    "pn13": {
+        "ending": "nôtre",
+        "declension": [{
+            "val": "nôtre","g": "m","n": "s","pe": 1
+        },{
+            "val": "nôtre","g": "f","n": "s","pe": 1
+        },{
+            "val": "nôtres","g": "m","n": "p","pe": 1
+        },{
+            "val": "nôtres","g": "f","n": "p","pe": 1
+        },{
+            "val": "vôtre","g": "m","n": "s","pe": 2
+        },{
+            "val": "vôtre","g": "f","n": "s","pe": 2
+        },{
+            "val": "vôtres","g": "m","n": "p","pe": 2
+        },{
+            "val": "vôtres","g": "f","n": "p","pe": 2
+        },{
+            "val": "leur","g": "m","n": "s","pe": 3
+        },{
+            "val": "leur","g": "f","n": "s","pe": 3
+        },{
+            "val": "leurs","g": "m","n": "p","pe": 3
+        },{
+            "val": "leurs","g": "f","n": "p","pe": 3
+        }]
+    },
+    "pn13-2": {
+        "ending": "vôtre",
+        "declension": [{
+            "val": "vôtre","g": "m","n": "s"
+        },{
+            "val": "vôtre","g": "f","n": "s"
+        },{
+            "val": "vôtres","g": "m","n": "p"
+        },{
+            "val": "vôtres","g": "f","n": "p"
+        }]
+    },
+    "pn13-3": {
+        "ending": "leur",
+        "declension": [{
+            "val": "leur","g": "m","n": "s"
+        },{
+            "val": "leur","g": "f","n": "s"
+        },{
+            "val": "leurs","g": "m","n": "p"
+        },{
+            "val": "leurs","g": "f","n": "p"
+        }]
+    },
+    
+}
