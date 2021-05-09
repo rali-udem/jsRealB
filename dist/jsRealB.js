ea0a6adc319436c20c355dc20a85f24f58752d54
lapalme <lapalme@iro.umontreal.ca>
2021-05-09 16:38:53 -0400

    Compile last changes to Constituent.js

--- dist/jsRealB.js
+++ dist/jsRealB.js
@@ -172,7 +172,6 @@ function genOptionFunc(option,validVals,allowedConsts,optionName){
             if (prog==undefined) this.addOptSource(option,val==null?undefined:val)
             return this;
         } else {
-            if (quoteOOV && this.isA("Q")) return this;
             return this.warn("bad const for option",option,this.constType,allowedConsts)
         }
     }
@@ -24019,7 +24018,7 @@ function testWarnings(){
         NP(D("un"),N("erreur")).warn(w,"A","B","C");
     }
 }
-jsRealB_dateCreated="2021-04-15 16:38"
+jsRealB_dateCreated="2021-05-09 16:38"
 //  Terminals
 exports.N=N;
 exports.A=A;
